efccf0b17ea5613bde41a7931fdd4f53d5a358dc
Stevan Little <stevan.little@iinteractive.com>
2013-09-13 13:09:12 -0400

    subscribe should always return a subscription, so lets make it easy on people

--- lib/React/Observable.pm
+++ lib/React/Observable.pm
@@ -3,14 +3,22 @@ use v5.16;
 use warnings;
 use mop;
 
+use Scalar::Util    qw[ blessed ];
 use Module::Runtime qw[ use_module ];
+
+use React::Subscription::Empty;
 
 class Observable {
 
-    has $!producer is ro;
+    has $!producer is ro = die '$!producer is required';
 
     method subscribe ($observer) {
-        $self->producer->( $observer );
+        my $subscription = $self->producer->( $observer );
+        return $subscription
+            if blessed $subscription
+            && $subscription->can('does')
+            && $subscription->does('React::Subscription');
+        return React::Subscription::Empty->new;
     }
 
     method take ( $n ) { use_module('React::Observable::Take')->new( sequence => $self, n => $n ) }
